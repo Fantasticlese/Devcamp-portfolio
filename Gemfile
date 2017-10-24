c40e59d032e0740a1175c515feb3ce32a2979668
Fantasticlese <joshuathorup@gmail.com>
2017-10-24 15:03:49 -0600

    Integrated new gem into rails app for copyright

--- Gemfile
+++ Gemfile
@@ -54,3 +54,5 @@ gem 'devise', '~> 4.3'
 
 gem 'bootstrap', '~> 4.0.0.beta2.1'
 gem 'jquery-rails', '~> 4.3', '>= 4.3.1'
+
+gem 'jthorup_view_tool', git: 'https://github.com/Fantasticlese/jthorup_view_tool'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
      		content_tag(:p, greeting, class: "source-greeting")
         end 
     end
+
+    def copyright_generator
+        JThorupViewTool::Renderer.copyright 'Josh Thorup', 'All rights reserved'
+    end
 end 
