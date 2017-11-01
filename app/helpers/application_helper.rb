8c35966e8743405d282454d915cad0536e315cfc
Fantasticlese <joshuathorup@gmail.com>
2017-11-01 16:31:31 -0600

    Refactored alerts into help method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -59,5 +59,18 @@ module ApplicationHelper
     def active? path
         "active" if current_page? path
     end
+
+    def alerts
+        alert = (flash[:alert] || flash[:error] || flash[:notice])
+    
+        if alert
+            alert_generator alert
+         end
+    end 
+
+    def alert_generator msg
+        js add_gritter(msg, title: "Josh Thorup Jedi Master", sticky: false, time: 2000)
+    end
+
 end 
 
